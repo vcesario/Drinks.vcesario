e09ab5982b81ee5ed93f1d057e180b4f826bf34c
victor cesario <victor.cesario@hotmail.com>
2025-03-07 08:21:06 -0300

    implemented get categories and get drink list

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,13 +1,38 @@
 using System.Text.Json;
-
-string url = "https://thecocktaildb.com/api/json/v1/1/list.php?c=list";
+using Spectre.Console;
 
 var client = new HttpClient();
 
-Stream stream = await client.GetStreamAsync(url);
+string url_categories = "https://thecocktaildb.com/api/json/v1/1/list.php?c=list";
+Stream stream = await client.GetStreamAsync(url_categories);
 var categoryList = JsonSerializer.Deserialize<DrinkCategoryList>(stream) ?? new(new());
-foreach (var category in categoryList.drinks)
+var prompt = new SelectionPrompt<DrinkCategory>()
+    .Title("Choose a drink category")
+    .AddChoices(categoryList.drinks)
+    .UseConverter(cat => cat.strCategory);
+prompt.AddChoice(new DrinkCategory("Exit"));
+var chosenCategory = AnsiConsole.Prompt(prompt);
+if (chosenCategory.strCategory.Equals("Exit"))
 {
-    Console.WriteLine(category.strCategory);
+    return;
 }
+
+string url_drinkbycategory = "https://thecocktaildb.com/api/json/v1/1/filter.php?c=";
+stream = await client.GetStreamAsync(url_drinkbycategory + chosenCategory.strCategory);
+var drinkList = JsonSerializer.Deserialize<DrinkPreviewList>(stream) ?? new(new());
+var prompt2 = new SelectionPrompt<DrinkPreview>()
+        .Title("Choose a drink")
+        .AddChoices(drinkList.drinks)
+        .UseConverter(drink => drink.strDrink);
+prompt2.AddChoice(new DrinkPreview("Return", "", "-1"));
+var chosenDrink = AnsiConsole.Prompt(prompt2);
+if (chosenDrink.strDrink.Equals("Return"))
+{
+    return;
+}
+
+string url_drinkdetails = "https://thecocktaildb.com/api/json/v1/1/lookup.php?i=";
+stream = await client.GetStreamAsync(url_drinkdetails + chosenDrink.idDrink);
+
+Console.WriteLine("Drink information...");
 Console.ReadLine();
